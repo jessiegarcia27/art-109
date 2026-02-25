f87730f3e3e30b7f1e091e7bf1ef54a24bbf7e0e
jessiegarcia27 <jessica.l.garcia01@sjsu.edu>
2026-02-25 12:48:44 -0800

    final

--- tutorials/2-css/07-DOM/js/main.js
+++ tutorials/2-css/07-DOM/js/main.js
@@ -5,6 +5,9 @@
 const header = document.querySelector("#header");
 const changeHeaderButton = document.querySelector("#change-header-button");
 const changeThemeButton = document.querySelector("#change-theme-button");
+const img1 = document.querySelector("#img1");
+const img2 = document.querySelector("#img2");
+const img3 = document.querySelector("#img3"); 
 
 
 //change header with button click
@@ -29,4 +32,14 @@ changeThemeButton.addEventListener("click", () => {
     //add remove dark class to body
     document.body.classList.toggle("dark");
 changeButtonText();
+})
+
+//toggle image visibility
+
+img1.addEventListener("click", () => {
+img2.classList.remove("hidden");
+})
+
+img2.addEventListener("click", () => {
+img3.classList.remove("hidden");
 })
