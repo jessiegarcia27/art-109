07db6a4fa88826cdde225fc27770b12fc32848fc
jessiegarcia27 <jessica.l.garcia01@sjsu.edu>
2026-02-25 10:36:02 -0800

    j

--- tutorials/2-css/07-DOM/js/main.js
+++ tutorials/2-css/07-DOM/js/main.js
@@ -1,2 +1,11 @@
-alert("JS loaded!");
-console.log("helloworld");
+// console.log("helloworld");
+
+
+//select html elements
+const header = document.querySelector("#header");
+const changeHeaderButton = document.querySelector("#change-header-button");
+
+//change header with button click
+changeHeaderButton.addEventListener("click", () => {
+header.innerHTML = "POW!!!";
+})
