796b9457d3c5c655d98f2724067594c703cf381c
jessiegarcia27 <jessica.l.garcia01@sjsu.edu>
2026-02-25 12:20:43 -0800

    d

--- tutorials/2-css/07-DOM/js/main.js
+++ tutorials/2-css/07-DOM/js/main.js
@@ -4,8 +4,29 @@
 //select html elements
 const header = document.querySelector("#header");
 const changeHeaderButton = document.querySelector("#change-header-button");
+const changeThemeButton = document.querySelector("#change-theme-button");
+
 
 //change header with button click
 changeHeaderButton.addEventListener("click", () => {
-header.innerHTML = "POW!!!";
+    header.innerHTML = "POW!!!";
+})
+
+//toggle color theme
+
+//create function for changing button text
+function changeButtonText() {
+    if (document.body.classList.contains("dark")) {
+        changeThemeButton.textContent = "Switch to Light Theme";
+    } else {
+        changeThemeButton.textContent = "Switch to Dark Theme";
+    }
+}
+
+
+//click event on button
+changeThemeButton.addEventListener("click", () => {
+    //add remove dark class to body
+    document.body.classList.toggle("dark");
+changeButtonText();
 })
