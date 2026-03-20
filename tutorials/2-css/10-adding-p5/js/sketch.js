e6e8546d3901ff579440475ee19c45a5ee482800
jessiegarcia27 <jessica.l.garcia01@sjsu.edu>
2026-03-19 20:38:39 -0700

    f

--- tutorials/2-css/10-adding-p5/js/sketch.js
+++ tutorials/2-css/10-adding-p5/js/sketch.js
@@ -1,27 +1,45 @@
 let canvas;
+let xPos = 0;
+let yPos = 0;
+let easing = .5;
+
 
 function setup () {
     canvas = createCanvas(windowWidth, windowHeight); 
     canvas.position(0, 0);
-    canvas.style('z-index', '-.5');
+  canvas.style('z-index', '-1');
     // background(125);
 }
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
 function draw () {
-    // strokeWeight(0);
-    // fill (random(200, 255), random(200, 255), random(200, 255));
-    // ellipse(mouseX, mouseY,30, 30); 
+ clear();
+
+  xPos = xPos + ((mouseX - xPos) * easing);
+  yPos = yPos + ((mouseY - yPos) * easing);
+
+    drawThings(xPos, yPos); 
+
 }
 
-function mouseMoved() {
-drawThings(mouseX, mouseY);
-drawThings(mouseX -50 , mouseY +75);
-}
 
-function drawThings (_x,_y) {
-          strokeWeight(0);
-    fill (random(200, 255), random(200, 255), random(200, 255));
-    ellipse(_x, _y,30, 30);  
+function drawThings (_x, _y) {
+
+  // heart color
+  fill(255, 0, 100);
+  noStroke();
+
+  // left bump
+  ellipse(_x - 10, _y - 10, 30, 30);
+
+  // right bump
+  ellipse(_x + 10, _y - 10, 30, 30);
+
+  // bottom point
+  triangle(
+    _x - 25, _y,
+    _x + 25, _y,
+    _x, _y + 40
+  );
 }
