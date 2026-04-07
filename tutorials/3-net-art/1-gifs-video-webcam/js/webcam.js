c296f4d8a855063dbb0bfe8269f74bf0b0c22b7e
jessiegarcia27 <jessica.l.garcia01@sjsu.edu>
2026-04-06 22:45:31 -0700

    Update webcam.js

--- tutorials/3-net-art/1-gifs-video-webcam/js/webcam.js
+++ tutorials/3-net-art/1-gifs-video-webcam/js/webcam.js
@@ -1,24 +1,16 @@
 const webcamVideo = document.querySelector('#webcam');
 
-
 async function setupWebcam() {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false
+    });
 
-    try {
-
-const stream = await navigator.mediaDevices.getUserMedia({ 
-    video: true,
-    audio: false
-
- });
-
-
- webcamVideo.srcObject = stream;
-
-
-    }catch (error) {
-        console.log("error accessing webcam: ");
-    }
-
+    webcamVideo.srcObject = stream;
+  } catch (error) {
+    console.log("error accessing webcam:", error);
+  }
 }
 
-startWebcam();
+setupWebcam();
