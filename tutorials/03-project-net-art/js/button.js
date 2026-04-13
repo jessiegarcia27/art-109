f9d4361b5cdbc489bd156d6c98b8974f5f41edb6
jessiegarcia27 <jessica.l.garcia01@sjsu.edu>
2026-04-12 19:35:36 -0700

    project

--- tutorials/03-project-net-art/js/button.js
+++ tutorials/03-project-net-art/js/button.js
@@ -15,38 +15,29 @@ input.addEventListener('input', () => {
     }, 500);
 });
 
-// const almostBtn = document.getElementById('almostBtn');
+const popupBtn = document.getElementById('popupBtn');
 
-// document.addEventListener('mousemove', () => {
-//     almostBtn.style.left = Math.random() * 500 + 'px';
-//     almostBtn.style.top = Math.random() * 500 + 'px';
-// });
+function createPopup() {
+    const popup = document.createElement('div');
+    popup.classList.add('popup');
 
+    popup.innerHTML = `
+    <p> are you sure you want to close this?</p>
+    <button class="closeBtn">Exit</button>
 
+    `;
+    document.body.appendChild(popup);
+    const closeBtn = popup.querySelector('.closeBtn');
+    closeBtn.addEventListener('click', () => {
+        popup.remove();
+    });
 
-// const almostBtn = document.getElementById('almostBtn');
-
-// document.addEventListener('mousemove', (e) => {
-//     const rect = almostBtn.getBoundingClientRect();
-
-//     const centerX = rect.left + rect.width / 2;
-//     const centerY = rect.top + rect.height / 2;
-
-//     const distanceX = Math.abs(e.clientX - centerX);
-//     const distanceY = Math.abs(e.clientY - centerY);
-
-//     if (distanceX < 150 && distanceY < 100) {
-//         const newX = Math.random() * (window.innerWidth - rect.width);
-//         const newY = Math.random() * (window.innerHeight - rect.height);
-//         almostBtn.style.left = newX + 'px';
-//         almostBtn.style.top = newY + 'px';
-//     }
-// });
-
-const almostBtn = document.getElementById('almostBtn');
-
-almostBtn.addEventListener('click', () => {
-    const messages = ["Nope!", "Try again!", "Almost there!", "You Wish!", "So close!"];
-    const randomMessage = messages[Math.floor(Math.random() * messages.length)];
-    almostBtn.textContent = randomMessage;
+setTimeout(() => {
+    createPopup();
+    createPopup ();
+}, 300);
+   }
+popupBtn.addEventListener("click", () => {
+    createPopup();
 });
+
--- tutorials/3-net-art/1-gifs-video-webcam/js/video.js
+++ tutorials/3-net-art/1-gifs-video-webcam/js/video.js
@@ -24,3 +24,4 @@ clickVideos.forEach(video => {
 
 
 });
+
