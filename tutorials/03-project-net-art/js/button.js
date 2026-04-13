3af7598fd1ac201f6bcdb2f5f83e62c6af8a9ac0
jessiegarcia27 <jessica.l.garcia01@sjsu.edu>
2026-04-12 21:17:55 -0700

    finished bad design project

--- tutorials/03-project-net-art/js/button.js
+++ tutorials/03-project-net-art/js/button.js
@@ -41,3 +41,76 @@ popupBtn.addEventListener("click", () => {
     createPopup();
 });
 
+const fakeProgressBtn = document.getElementById('fakeProgressBtn');
+
+fakeProgressBtn,addEventListener('click', () => {
+    const loader = document.createElement('div');
+    loader.classList.add('loader');
+
+    loader.innerHTML = `
+    <p>Loading something important...</p>
+        <div class="bar">
+            <div class="progress"></div>
+        </div>
+    `;
+    
+    document.body.appendChild(loader);
+    const progress = loader.querySelector('.progress');
+    let width = 0;
+
+    const interval = setInterval(() => {
+        if (width <95) {
+            width += Math.random() * 5;
+            progress.style.width = width + '%';
+    
+        } else {
+            clearInterval(interval);
+
+            setTimeout(() => {
+                progress.innerText = "almost done....";
+            }, 500);
+        }
+    }, 200);
+    });
+
+    const ghost = document.getElementById('ghostCursor');
+    let ghostX = 0;
+    let ghostY = 0;
+
+    document.addEventListener('mousemove', (e) => {
+        const targetX = e.clientX;
+        const targetY = e.clientY;
+
+        setInterval(() => {
+          ghostX += (targetX - ghostX) * 0.05;
+        ghostY += (targetY - ghostY) * 0.05;
+
+        ghost.style.left = ghostX + 'px';
+        ghost.style.top = ghostY + 'px';
+        }, 10);
+    });
+
+const img = document.getElementById('reactImage');
+
+img.addEventListener("mouseover", () => {
+    img.style.transform = "scale(1.2)";
+});
+
+img.addEventListener("mouseout", () => {
+    img.style.transform = "scale(1)";
+});
+
+img.addEventListener("click", () => {
+    const x = (Math.random() - 0.5) * 40;
+    const y = (Math.random() - 0.5) * 40;
+
+    img.style.transform = `translate(${x}px, ${y}px) scale(1.1)`;
+});
+
+
+
+
+
+        
+
+
